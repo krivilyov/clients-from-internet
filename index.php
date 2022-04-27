fdb0822e771c9bd27ab7167f4aa5f6e10fa4771c
Vanya Krivilyov <ivan.k@miro.com>
2022-04-27 21:54:10 +0400

    added pages and styles

--- index.php
+++ index.php
@@ -2,4 +2,15 @@
 	$db = new PDO('mysql:host=db;port=3306;dbname=clients-from-internet', 'root', 'root');
 //	var_dump($db->getAttribute(PDO::ATTR_CONNECTION_STATUS));
 
-	include ('templates/login.html');
+//	if(isset($_POST['email'])){
+//		var_dump($_POST); die;
+//	}
+
+	//entry point
+	if($_GET['action'] === "login"){
+		include ('templates/login.html');
+	} else {
+		include ('templates/index.html');
+	}
+
+
